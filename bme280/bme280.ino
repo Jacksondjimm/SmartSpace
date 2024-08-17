71f8579cb5ed1134bc7d1ce55b5a422f22d6aa3a
Evgeny <jacksondjimm@yandex.ru>
2024-08-17 20:24:19 +0300

    test

--- bme280/bme280.ino
+++ bme280/bme280.ino
@@ -15,8 +15,8 @@ Adafruit_BME280 bme;
 const char* ssid = "Name";                   // Название WIFI сети
 const char* password = "your password";                // Пароль от WIFI сети
 
-const char* host = "192.168.0.770";       // Локальный адрес
-//const char* host = "88.201.252.176";        // глобальный адрес
+//const char* host = "192.168.0.110";       // Локальный адрес - читается с#
+const char* host = "88.201.252.176";        // глобальный адрес - читается с#
 const uint16_t port = 80;                 // Порт
 
 //float num = 11111; // для BME280 №01 your secret number
@@ -25,7 +25,7 @@ float num = 22222; // для BME280 №02 your secret number
 //float num = 44444; // для BME280 №04 your secret number
 //float num = 55555; // для BME280 №05 your secret number
 
-
+float Ktemp = 1.5; // коэф.калибровки аддитивной погрешности
 
 //#define DHTPIN 2 
 //#define DHTTYPE    DHT11     // DHT 11
@@ -58,7 +58,7 @@ void setup()
 
 void loop() {
 
-  delay(60000);
+  delay(300000);
   
   // old code begin
   /*sensors_event_t event;
@@ -78,12 +78,12 @@ void loop() {
     //client.print("GET /insert.php?");
     client.print("GET /index?");//аналог строки выше, но для C#
     client.print("temp=");
-    client.print(temp);
+    client.print(temp-Ktemp);
     client.print("&hum=");
     client.print(hum);
     client.print("&num=");
     client.print(num);    
-    client.println(" HTTP/1.1");
+    client.println(" HTTP/1.1"); 
     client.print( "Host: " );
     client.println(host);
     client.println( "Connection: close" );
@@ -105,5 +105,5 @@ void loop() {
   {
     client.stop();                                    // Ошибка подключения
   }
-  delay(10000);                         // Ждём 10 мекунд
+  delay(10000);                         // Ждём 10 cекунд
 }
